f828ec55cd8a620fce7671da2b2ff82aa780de2b
Alexandre Mutel <alexandre_mutel@live.com>
2016-04-08 23:48:49 +0900

    Add other popular web frameworks

--- src/NUglify.Tests/JavaScript/JsPopular.cs
+++ src/NUglify.Tests/JavaScript/JsPopular.cs
@@ -2,28 +2,72 @@
 // This file is licensed under the BSD-Clause 2 license. 
 // See the license.txt file in the project root for more information.
 
+using System;
 using System.Net;
-using System.Net.Http;
 using NUglify.JavaScript;
 using NUnit.Framework;
 
 namespace NUglify.Tests.JavaScript
 {
+    /// <summary>
+    /// Tests NUglify Javsscript minifier on several popular web frameworks 
+    /// We mainly test that parser is working without any errors.
+    /// </summary>
     [TestFixture]
     public class JsPopular
     {
         [Test]
         public void TestJQuery()
         {
-            var client = new WebClient();
             var jqueryVersion = "jquery-2.2.3.js";
-            var jqueryText = client.DownloadString($"https://code.jquery.com/{jqueryVersion}");
-            var result = Uglify.Js(jqueryText, jqueryVersion, new CodeSettings()
+            AssertCompile($"https://code.jquery.com/{jqueryVersion}", jqueryVersion);
+        }
+
+        [Test]
+        public void TestAngular()
+        {
+            var file = "angular-1.5.3.js";
+            AssertCompile($"https://ajax.googleapis.com/ajax/libs/angularjs/1.5.3/angular.js", file);
+        }
+
+        [Test]
+        public void TestHandlebars()
+        {
+            var file = "handlebars-v4.0.5.js";
+            AssertCompile($"http://builds.handlebarsjs.com.s3.amazonaws.com/{file}", file);
+        }
+
+        [Test]
+        public void TestReact()
+        {
+            var file = "react-15.0.0.js";
+            AssertCompile($"https://fb.me/{file}", file);
+            file = "react-dom-15.0.0.js";
+            AssertCompile($"https://fb.me/{file}", file);
+        }
+
+        [Test]
+        public void TestEmber()
+        {
+            var file = "ember.prod-v2.4.4.js";
+            AssertCompile($"http://builds.emberjs.com/tags/v2.4.4/ember.prod.js", file);
+        }
+
+        private static void AssertCompile(string url, string file)
+        {
+            // https://ajax.googleapis.com/ajax/libs/angularjs/1.5.3/angular.js
+            var client = new WebClient();
+            var jqueryText = client.DownloadString(url);
+            var result = Uglify.Js(jqueryText, file, new CodeSettings()
             {
                 PreserveImportantComments = false,
                 StripDebugStatements = true,
                 LineBreakThreshold = int.MaxValue,
             });
+            foreach (var error in result.Errors)
+            {
+                Console.WriteLine(error.ToString());
+            }
             Assert.False(result.HasErrors);
         }
     }
