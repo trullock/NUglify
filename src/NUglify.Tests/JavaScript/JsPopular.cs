d23644f339ff2601f709a14f1d57fd2697f97902
Alexandre Mutel <alexandre_mutel@live.com>
2016-04-08 23:33:42 +0900

    Add parsing jquery as a test

---
+++ src/NUglify.Tests/JavaScript/JsPopular.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) Alexandre Mutel. All rights reserved.
+// This file is licensed under the BSD-Clause 2 license. 
+// See the license.txt file in the project root for more information.
+
+using System.Net;
+using System.Net.Http;
+using NUglify.JavaScript;
+using NUnit.Framework;
+
+namespace NUglify.Tests.JavaScript
+{
+    [TestFixture]
+    public class JsPopular
+    {
+        [Test]
+        public void TestJQuery()
+        {
+            var client = new WebClient();
+            var jqueryVersion = "jquery-2.2.3.js";
+            var jqueryText = client.DownloadString($"https://code.jquery.com/{jqueryVersion}");
+            var result = Uglify.Js(jqueryText, jqueryVersion, new CodeSettings()
+            {
+                PreserveImportantComments = false,
+                StripDebugStatements = true,
+                LineBreakThreshold = int.MaxValue,
+            });
+            Assert.False(result.HasErrors);
+        }
+    }
+}
