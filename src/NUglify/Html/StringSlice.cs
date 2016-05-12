739de87f57bfdd0a2a9f4684c331710aa80abec0
Alexandre Mutel <alexandre_mutel@live.com>
2016-05-12 23:09:41 +0900

    Add StringSlice helper methods to Trim spaces

--- src/NUglify/Html/StringSlice.cs
+++ src/NUglify/Html/StringSlice.cs
@@ -2,15 +2,15 @@
 // This file is licensed under the BSD-Clause 2 license. 
 // See the license.txt file in the project root for more information.
 
+using NUglify.Helpers;
+
 namespace NUglify.Html
 {
     public struct StringSlice
     {
-        public StringSlice(string text)
+        public StringSlice(string text) : this()
         {
-            Text = text;
-            Start = text == null ? 1 : 0;
-            End = text != null ? text.Length - 1 : 0;
+            ChangeText(text);
         }
 
         public StringSlice(string text, int start, int end)
@@ -20,13 +20,20 @@ namespace NUglify.Html
             End = end;
         }
 
-        public string Text { get; }
+        public string Text { get; private set; }
 
         public int Start { get; set; }
 
         public int End { get; set; }
 
         public int Length => End - Start + 1;
+
+        public void ChangeText(string text)
+        {
+            Text = text;
+            Start = text == null ? 1 : 0;
+            End = text != null ? text.Length - 1 : 0;
+        }
 
         public override string ToString()
         {
@@ -36,6 +43,82 @@ namespace NUglify.Html
         public bool StartsBySpace()
         {
             return Start <= End && Text != null && Text[Start].IsSpace();
+        }
+
+        public void TrimStart()
+        {
+            if (Text == null)
+            {
+                return;
+            }
+
+            for (; Start <= End; Start++)
+            {
+                if (!Text[Start].IsSpace())
+                {
+                    break;
+                }
+            }
+        }
+
+        public void CollapseSpaces()
+        {
+            if (Text == null)
+            {
+                return;
+            }
+
+            // TODO: this method could be optimized by checking if 
+            // it is really necessary to perform a StringBuilder
+            // if there are no spaces to collapse
+            var builder = StringBuilderPool.Acquire();
+            bool previousWasSpace = false;
+            for (int i = Start; i <= End; i++)
+            {
+                var c = Text[i];
+                if (c.IsSpace())
+                {
+                    if (!previousWasSpace)
+                    {
+                        builder.Append(c);
+                    }
+                    previousWasSpace = true;
+                }
+                else
+                {
+                    builder.Append(c);
+                    previousWasSpace = false;
+                }
+            }
+
+            ChangeText(builder.ToString());
+
+            builder.Release();
+        }
+
+        public void TrimEnd()
+        {
+            if (Text == null)
+            {
+                return;
+            }
+
+            for (; End >= Start; End--)
+            {
+                if (!Text[End].IsSpace())
+                {
+                    break;
+                }
+            }
+        }
+
+        public bool HasTrailingSpaces()
+        {
+            if (Text == null)
+            {
+                return false;
+            }
+            return Start <= End && Text[End].IsSpace();
         }
 
         public bool IsEmptyOrWhiteSpace()
