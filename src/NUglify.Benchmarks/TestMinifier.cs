289d88f2f51fd8d4d4c550e898ae41a5559df418
Alexandre Mutel <alexandre_mutel@live.com>
2016-05-15 00:10:40 +0900

    Add Benchmarks comparison against AngleSharp (for parsing), and HtmlCompressor (for minifier)

---
+++ src/NUglify.Benchmarks/TestMinifier.cs
@@ -0,0 +1,71 @@
+﻿// Copyright (c) Alexandre Mutel. All rights reserved.
+// This file is licensed under the BSD-Clause 2 license. 
+// See the license.txt file in the project root for more information.
+
+using System;
+using System.IO;
+using System.Net;
+using BenchmarkDotNet.Attributes;
+using NUglify.Html;
+using ZetaProducerHtmlCompressor;
+
+namespace NUglify.Benchmarks
+{
+    public class TestMinifier
+    {
+        private readonly string html;
+
+        public TestMinifier()
+        {
+            var htmlFile = Path.Combine(Path.GetDirectoryName(typeof(TestMinifier).Assembly.Location), @"..\HTMLStandard.htm");
+            if (!File.Exists(htmlFile))
+            {
+                Console.WriteLine("Downloading https://html.spec.whatwg.org/");
+                using (var webClient = new WebClient())
+                {
+                    html = webClient.DownloadString("https://html.spec.whatwg.org/");
+                }
+                File.WriteAllText(htmlFile, html);
+            }
+            else
+            {
+                html = File.ReadAllText(htmlFile);
+            }
+        }
+
+        [Benchmark]
+        public void BenchNUglify()
+        {
+            var result = Uglify.Html(html, new HtmlSettings() { MinifyJs = false, MinifyCss = false });
+            var size = result.Code.Length;
+            //File.WriteAllText(
+            //    Path.Combine(Path.GetDirectoryName(typeof(Program).Assembly.Location), "HTMLStandard.min.htm"),
+            //    result.Code);
+        }
+
+        [Benchmark]
+        public void BenchHtmlCompressor()
+        {
+            var compressor = new HtmlContentCompressor();
+            var result = compressor.Compress(html);
+            var size = result.Length;
+            //File.WriteAllText(
+            //    Path.Combine(Path.GetDirectoryName(typeof(Program).Assembly.Location), "HTMLStandard.min3.htm"),
+            //    result);
+        }
+
+        // WebMarkupMin doesn't work
+
+        //[Benchmark]
+        //public void BenchWebMarkupMin()
+        //{
+        //    var minifier = new WebMarkupMin.Core.HtmlMinifier();
+        //    var result = minifier.Minify(html);
+        //    var size = result.MinifiedContent.Length;
+
+        //    File.WriteAllText(
+        //        Path.Combine(Path.GetDirectoryName(typeof(Program).Assembly.Location), "HTMLStandard.min2.htm"),
+        //        result.MinifiedContent);
+        //}
+    }
+}
