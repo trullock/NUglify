63fe3fc9e79d1c0bc10e28cdde21ed536b6d0fc0
Alex Raileanu <alexr@gabriels.net>
2020-03-16 17:36:28 +0200

    Fixes issues with multiple await return.
    Fixes #68

---
+++ src/NUglify.Tests/TestData/JS/Input/ES2017/MultipleAwaitExpression.js
@@ -0,0 +1,16 @@
+﻿async function getNumberAfterTimeout(value) {
+    return new Promise((resolve) => {
+        window.setTimeout(() => resolve(value), 2000);
+    });
+}
+
+async function sum(x) {
+    var a = getNumberAfterTimeout(20);
+    var b = getNumberAfterTimeout(30);
+    return x + await a + await b;
+}
+
+(async () => { return x + await a(t) + await b; })()
+
+// keep grouping
+(async () => { return x + (await a(t) + await b); })()
