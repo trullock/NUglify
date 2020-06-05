1f7608e1e7c52b152ea2d8649f036dc4607a867c
Andrew Bullock <trullock@gmail.com>
2020-06-05 17:29:32 +0100

    fixed test assertion

--- src/NUglify.Tests/Core/SymbolsMapTests.cs
+++ src/NUglify.Tests/Core/SymbolsMapTests.cs
@@ -44,7 +44,7 @@ namespace NUglify.Tests.Core
             // shouldn't be any linebreaks other than that.
             var indexLineBreak = minifiedCode.IndexOf('\n');
             var trimmedCode = indexLineBreak < 0 ? minifiedCode : minifiedCode.Substring(0, indexLineBreak);
-            Assert.AreEqual("function globalFunc(n){alert(n)}var foo=42;var arrow=e=>{return e*2}", trimmedCode, "minified code not expected");
+            Assert.AreEqual("function globalFunc(n){alert(n)}var foo=42,arrow=n=>n*2", trimmedCode, "minified code not expected");
 
             // verify that the minified code has the sourceMappingURL directive in it
             Assert.IsTrue(minifiedCode.Contains("sourceMappingURL=" + mapPath), "sourceMappingURL should be in minified content");
