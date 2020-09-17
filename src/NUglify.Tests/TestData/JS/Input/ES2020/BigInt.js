dc49137e68e8d5e43fd8b1b5efca84d5179613cd
Andrew Bullock <trullock@gmail.com>
2020-09-17 11:49:31 +0100

    added failing tests for bigint

---
+++ src/NUglify.Tests/TestData/JS/Input/ES2020/BigInt.js
@@ -0,0 +1,13 @@
+﻿const theBiggestInt = 9007199254740991n;
+
+const alsoHuge = BigInt(9007199254740991);
+// ↪ 9007199254740991n
+
+const hugeString = BigInt("9007199254740991");
+// ↪ 9007199254740991n
+
+const hugeHex = BigInt("0x1fffffffffffff");
+// ↪ 9007199254740991n
+
+const hugeBin = BigInt("0b11111111111111111111111111111111111111111111111111111");
+// ↪ 9007199254740991n
