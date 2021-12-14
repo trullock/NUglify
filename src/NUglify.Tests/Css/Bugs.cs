d8693fc7e69b7e1b22c81e0ae05cc1ba2b9d6b1b
Andrew Bullock <trullock@gmail.com>
2021-12-14 13:09:26 +0000

    fixes #283

--- src/NUglify.Tests/Css/Bugs.cs
+++ src/NUglify.Tests/Css/Bugs.cs
@@ -66,17 +66,17 @@ namespace NUglify.Tests.Css
         [Test]
         public void Bug280()
         {
-            Assert.AreEqual(":is(.container) :is(.bold){font-weight:bold;}", Uglify.Css(":is(.container) :is(.bold) { font-weight: bold; }").Code);            
+            Assert.AreEqual(":is(.container) :is(.bold){font-weight:bold}", Uglify.Css(":is(.container) :is(.bold) { font-weight: bold; }").Code);            
 
-            Assert.AreEqual(":is(.container):is(.bold){font-weight:bold;}", Uglify.Css(":is(.container):is(.bold) { font-weight: bold; }").Code);
+            Assert.AreEqual(":is(.container):is(.bold){font-weight:bold}", Uglify.Css(":is(.container):is(.bold) { font-weight: bold; }").Code);
 
-            Assert.AreEqual(".container :is(.bold){font-weight:bold;}", Uglify.Css(".container :is(.bold) { font-weight: bold; }").Code);
+            Assert.AreEqual(".container :is(.bold){font-weight:bold}", Uglify.Css(".container :is(.bold) { font-weight: bold; }").Code);
 
             // Assert whether extra unnecessary spaces around parentheses are removed:
-            Assert.AreEqual(":is(.container) :is(.bold){font-weight:bold;}", Uglify.Css(":is(.container ) :is(.bold ) { font-weight: bold; }").Code);
+            Assert.AreEqual(":is(.container) :is(.bold){font-weight:bold}", Uglify.Css(":is(.container ) :is(.bold ) { font-weight: bold; }").Code);
 
             // Assert a more extensive scenario:
-            Assert.AreEqual("body{font-family:'Franklin Gothic Medium','Arial Narrow',Arial,sans-serif}:root{--blue:'blue';--five:5px;--width:100px;--half-width:calc(var(--width)/2);--half-width-with-indent:calc(var(--half-width) + 16px);--half-width-with-indent:calc(10% + var(--half-width) + 16px)}.margin{margin:calc(2*var(--five));padding:var(--half-width-with-indent)}:is(.container) :is(.bold){font-weight:bold;}:is(.container) :where(.bold){grid-template-areas:'myArea myArea myArea myArea myArea';}div :is(.test),div:is(.test){color:var(--blue);}",
+            Assert.AreEqual("body{font-family:'Franklin Gothic Medium','Arial Narrow',Arial,sans-serif}:root{--blue:'blue';--five:5px;--width:100px;--half-width:calc(var(--width)/2);--half-width-with-indent:calc(var(--half-width) + 16px);--half-width-with-indent:calc(10% + var(--half-width) + 16px)}.margin{margin:calc(2*var(--five));padding:var(--half-width-with-indent)}:is(.container) :is(.bold){font-weight:bold}:is(.container) :where(.bold){grid-template-areas:'myArea myArea myArea myArea myArea'}div :is(.test),div:is(.test){color:var(--blue)}",
                  Uglify.Css(@"body {
     font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
 }
