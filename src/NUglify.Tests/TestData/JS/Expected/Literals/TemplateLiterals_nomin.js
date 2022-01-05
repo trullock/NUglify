477f1c6afcdb1be04c65627fd00bf4374d56f1d2
Julian Dominguez <julido@microsoft.com>
2022-01-05 20:20:54 -0300

    Do not break escaping of dollar sign in template literals
    Fixes #291

--- src/NUglify.Tests/TestData/JS/Expected/Literals/TemplateLiterals_nomin.js
+++ src/NUglify.Tests/TestData/JS/Expected/Literals/TemplateLiterals_nomin.js
@@ -1,4 +1,4 @@
 ﻿var nosub=`now is the time for all good men`,nosubFunc=safehtml`<a href="#">h\x65ad</a>`,multiline=`now is the time \
     for all good men
     to come to the aid
-    of their country.`,total=34.99,msg=`Total = ${total}, plus tax is ${total*1.098}`,esc=`\u0ca0_\u{ca0}`;esc=`template \` with backtick`;var reg=new RegExp(`<[\\w-\\d]$`,"i"),obj=`Via ${{http:"http",https:"SSL",file:"file system"}[location.protocol]}!`,nested=`one two ${`double my total is ${2*total}`} three`
+    of their country.`,total=34.99,msg=`Total = ${total}, plus tax is ${total*1.098}`,esc=`\u0ca0_\u{ca0}`;esc=`template \` with backtick`;esc=`template \$ with escaped dolar sign`;esc=`template \${^^^^this isn't code@#%&} with escaped dolar sign followed by braces`;var reg=new RegExp(`<[\\w-\\d]$`,"i"),obj=`Via ${{http:"http",https:"SSL",file:"file system"}[location.protocol]}!`,nested=`one two ${`double my total is ${2*total}`} three`
